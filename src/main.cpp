6e16d8addcc59db75337a64a61a8f2e196de6177
ravi <ravjkant.gla_cs.aiml24@gla.ac.in>
2026-02-20 15:07:56 +0530

    add TODO comments for distance measurement implementation

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,36 @@
 
 void setup() {
 
+    // TODO 5:
+    // Initialize Serial communication (9600 baud rate)
 
+    // TODO 6:
+    // Configure TRIG as OUTPUT
+
+    // TODO 7:
+    // Configure ECHO as INPUT
+
+    // TODO 8:
+    // Print system initialization message
 }
 
 void loop() {
 
+    // TODO 9:
+    // Set TRIG LOW for 2 microseconds
 
+    // TODO 10:
+    // Send 10 microsecond pulse on TRIG
+
+    // TODO 11:
+    // Measure pulse duration on ECHO using pulseIn()
+
+    // TODO 12:
+    // Calculate distance in cm
+
+    // TODO 13:
+    // Print calculated distance
+
+    // TODO 14:
+    // Add delay (500ms)
 }
